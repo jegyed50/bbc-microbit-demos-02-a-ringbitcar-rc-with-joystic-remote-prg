d37a3b446ad84d182d108ad2e47250065e5df829
Egyed József <jegyed50@gmail.com>
2023-05-23 16:59:10 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-basic.showIcon(IconNames.Heart)
+let Y = 0
+let X = 0
+radio.setGroup(90)
+let szorzo = 3
+music.setVolume(255)
+basic.forever(function () {
+    X = pins.analogReadPin(AnalogPin.P0)
+    X = Math.map(X, 1023, 0, -100, 100)
+    radio.sendValue("x", X)
+    Y = pins.analogReadPin(AnalogPin.P1)
+    Y = Math.map(Y, 1023, 0, -100, 100)
+    radio.sendValue("y", Y)
+    basic.pause(10)
+})
